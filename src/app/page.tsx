38232b85a3e5def66ef9cd6a5cf8792c49099a6b
FasihAhmedRaza <realfasih@gmail.com>
2024-04-13 13:13:12 +0500

    remove Reviwes.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@
 import HeroSection from "@/components/HeroSection";
 import CardStackDemo from "@/components/Cardsection";
 import NewSection from "@/components/Newsection";
-import Reviews from "@/components/Reviews";
 import Skills from "@/components/Skills";
 import UpcomingWebinars from "@/components/ui/UpcomingWebinars";
-import { Calendar } from "lucide-react";
-
 
 export default function Home() {
   return (
