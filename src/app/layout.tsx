dc8b73f52bb7df728bc9921c52a27ab97b070abe
FasihAhmedRaza <realfasih@gmail.com>
2024-04-15 00:30:22 +0500

    fixed errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { FloatingNav } from "@/components/Nava";
 import Footer from "@/components/Footer";
 import  CookieConsentModal  from "@/components/CookieConsentModal";
 import CalendarModal from "@/components/Calender";
+import Link from "next/link";
 
 
 const inter = Inter({ subsets: ["latin"] });
